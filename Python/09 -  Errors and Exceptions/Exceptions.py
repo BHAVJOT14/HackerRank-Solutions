7380a23185dcb48ecba9e69c11b102b8a33ce796
bhavi140201 <68502679+bhavi140201@users.noreply.github.com>
2021-08-24 07:24:22 +0530

    Completed Errors and Exceptions Sub - Domain

---
+++ Python/09 -  Errors and Exceptions/Exceptions.py
@@ -0,0 +1,9 @@
+
+for i in range(int(input())):
+    try:
+        a, b = input().split()
+        print(int(a) // int(b))
+    except ZeroDivisionError as z:
+        print(f'Error Code: {z}')
+    except ValueError as v:
+        print(f'Error Code: {v}')
---
+++ Python/09 -  Errors and Exceptions/Incorrect Regex.py
@@ -0,0 +1,9 @@
+
+import re
+
+for i in range(int(input())):
+    try:
+        a = re.compile(input())
+        print("True")
+    except Exception:
+        print("False")
